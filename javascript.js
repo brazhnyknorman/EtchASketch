d62af915108c5efd1878067bbc9845cef95b73c2
brazhnyknorman <brazhnyknorman@gmail.com>
2024-06-19 09:56:05 -0400

    Add basic div to contain future squares

---
+++ javascript.js
@@ -0,0 +1,11 @@
+/* Defines container*/
+const container = document.querySelector(".container");
+const body = document.querySelector("body");
+
+/* Creates button element to adjust grid*/
+const adjust = document.createElement("button");
+
+/* Defines array to hold the elements*/
+const divArray = [];
+
+
